dae22d5954e050c44ebf7707ff96bfb05d662b4f
kylin <kylinsoong.1214@gmail.com>
2017-07-08 03:29:10 +0800

    add a tomcat version

---
+++ src/main/java/org/openshift/workshop/ui/controller/CXFClient.java
@@ -0,0 +1,103 @@
+package org.openshift.workshop.ui.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.openshift.workshop.ui.model.*;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SuppressWarnings("unchecked")
+public class CXFClient {
+    
+    private static String BASE = buildBaseURL();
+    
+    private static String API_PRODUCT = BASE + "/rest/query/account/products";
+    private static String API_MARKETDATA = BASE + "/rest/query/marketData/stockPrices";
+    private static String API_STOCKS= BASE + "/rest/query/stocks";
+    
+    
+    public static List<Product> loadProduct(final List<Product> items) {
+        
+        try {
+            WebClient wc = WebClient.create(API_PRODUCT);
+            Response resp = wc.get();
+            InputStream in = (InputStream) resp.getEntity();
+            ObjectMapper mapper = new ObjectMapper();
+            List<Map<String, Object>> list = mapper.readValue(in, List.class);
+            list.forEach(m -> {
+                Integer id = (Integer) m.get("id");
+                String symbol = (String) m.get("symbol");
+                String company_name = (String) m.get("company_name") ;
+                items.add(new Product(id, symbol, company_name));
+            });
+            wc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return items;
+    }
+    
+    private static String buildBaseURL() {
+        
+        String base = System.getProperty("REST_PROTOCOL", "http") + "://" + System.getProperty("REST_HOST", "spring-boot-cxf-jaxrs") + ":" +  System.getProperty("REST_PORT", "8080");
+        
+        return "http://spring-boot-cxf-jaxrs-sample.apps.na1.openshift.opentlc.com";
+    }
+
+    public static List<StockPrice> loadMarketData(final List<StockPrice> items) {
+        try {
+            WebClient wc = WebClient.create(API_MARKETDATA);
+            Response resp = wc.get();
+            InputStream in = (InputStream) resp.getEntity();
+            ObjectMapper mapper = new ObjectMapper();
+            List<Map<String, Object>> list = mapper.readValue(in, List.class);
+            list.forEach(m -> {
+                String symbol = (String) m.get("symbol");
+                Double price = (Double) m.get("price") ;
+                items.add(new StockPrice(symbol, price));
+            });
+            wc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    public static List<Stock> loadStocks(final List<Stock> items) {
+        try {
+            WebClient wc = WebClient.create(API_STOCKS);
+            Response resp = wc.get();
+            InputStream in = (InputStream) resp.getEntity();
+            ObjectMapper mapper = new ObjectMapper();
+            List<Map<String, Object>> list = mapper.readValue(in, List.class);
+            list.forEach(m -> {
+                Integer id = (Integer) m.get("id");
+                String symbol = (String) m.get("symbol");
+                Double price = (Double) m.get("price") ;
+                String company_name = (String) m.get("company_name") ;
+                items.add(new Stock(id, symbol, price, company_name));
+            });
+            wc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+        
+       System.out.println(CXFClient.loadProduct(new ArrayList<Product>()));
+       System.out.println(CXFClient.loadMarketData(new ArrayList<StockPrice>()));
+       System.out.println(CXFClient.loadStocks(new ArrayList<Stock>()));
+    }
+}
---
+++ src/main/java/org/openshift/workshop/ui/controller/RestController.java
@@ -0,0 +1,46 @@
+package org.openshift.workshop.ui.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+import org.openshift.workshop.ui.model.Product;
+import org.openshift.workshop.ui.model.Stock;
+import org.openshift.workshop.ui.model.StockPrice;
+
+@RequestScoped
+public class RestController {
+
+    private List<Product> products = new ArrayList<>();
+    private List<StockPrice> stockPrices = new ArrayList<>();
+    private List<Stock> stocks = new ArrayList<>();
+
+    @Produces
+    @Named
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Produces
+    @Named
+    public List<StockPrice> getStockPrices() {
+        return stockPrices;
+    }
+
+    @Produces
+    @Named
+    public List<Stock> getStocks() {
+        return stocks;
+    }
+    
+    @PostConstruct
+    public void invokeRest() {
+        CXFClient.loadProduct(products);
+        CXFClient.loadMarketData(stockPrices);
+        CXFClient.loadStocks(stocks);
+    }
+}
